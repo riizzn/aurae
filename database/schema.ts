4b9429ce151f3d32504e3284a33a9543b46fa5f4
rana <ranabdulsalam@gmail.com>
2025-07-02 00:11:04 +0530

    drizzle schema

---
+++ database/schema.ts
@@ -0,0 +1,39 @@
+
+import {
+  pgEnum,
+  date,
+  integer,
+  text,
+  boolean,
+  pgTable,
+  uuid,
+  varchar,
+  timestamp,
+} from "drizzle-orm/pg-core";
+
+export const STATUS_ENUM = pgEnum("status", [
+  "PENDING",
+  "APPROVED",
+  "REJECTED",
+]);
+export const ROLE_ENUM = pgEnum("role", ["USER", "ADMIN"]);
+export const BORROW_STATUS_ENUM = pgEnum("borrow_status", [
+  "BORROWED",
+  "RETURNED",
+]);
+
+export const users = pgTable("users", {
+  id: uuid("id").notNull().primaryKey().defaultRandom().unique(),
+  fullName: varchar("full_name", { length: 255 }).notNull(),
+  email: text("email").notNull().unique(),
+  auraeId: integer("aurae_id").notNull().unique(),
+  password: text("password").notNull(),
+  auraeCard: text("aurae_card").notNull(),
+  status: STATUS_ENUM('status').default('PENDING'),
+  role:ROLE_ENUM('role').default('USER'),
+  lastActivityDate:date('last_activity_date').defaultNow(),
+  createdAt:timestamp('created_at', {
+    withTimezone:true,
+  }).defaultNow
+
+});
