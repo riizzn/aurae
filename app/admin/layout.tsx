56186c19f7ef6133c2272aef390fc7c74fc95a88
rana <ranabdulsalam@gmail.com>
2025-07-09 16:13:20 +0530

    book action

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,27 +2,34 @@ import React, { ReactNode } from "react";
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 
-
-
 import Sidebar from "@/components/admin/Sidebar";
 import Header from "@/components/admin/Header";
-
+import { users } from "@/database/schema";
+import { db } from "@/database/drizzle";
+import { eq } from "drizzle-orm";
 
 const Layout = async ({ children }: { children: ReactNode }) => {
   const session = await auth();
 
   if (!session?.user?.id) redirect("/sign-in");
+  const isAdmin = await db
+    .select({ isAdmin: users.role })
+    .from(users)
+    .where(eq(users.id, session.user.id))
+    .limit(1)
+    .then((res) => res[0]?.isAdmin === "ADMIN");
 
+    if(!isAdmin){
+      redirect("/")
 
+    }
 
   return (
     <main className="flex min-h-screen w-full flex-row">
-      
-         <Sidebar session={session}/>
+      <Sidebar session={session} />
       <div className="admin-container">
-        <Header session={session}/>
-        
-      
+        <Header session={session} />
+
         {children}
       </div>
     </main>
